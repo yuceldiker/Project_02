fa3df92ffc60b588c03554f941babee3652f98d9
Yücel <yuceldiker@gmail.com>
2020-08-28 12:13:50 +0300

    Add project files.

---
+++ Project2/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Project2.Models;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Project2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MobildevEntities db = new MobildevEntities();
+        private SqlConnection con;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetUsers()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            var users = db.Users.ToList();
+            return Json(new { data = users }, JsonRequestBehavior.AllowGet);
+        }
+
+        public void AddUser(User obj)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["SqlCon"].ToString();
+            con = new SqlConnection(constr);
+
+            SqlCommand connect = new SqlCommand("SP_user_Add", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            connect.Parameters.AddWithValue("@FirstName", obj.FirstName);
+            connect.Parameters.AddWithValue("@LastName", obj.LastName);
+            connect.Parameters.AddWithValue("@Phone", obj.Phone);
+            connect.Parameters.AddWithValue("@Email", obj.Email);
+            con.Open();
+            connect.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
